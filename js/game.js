978688f98c00130620e944daef44b6f3cf531ae5
Mercedes Egido <meegido@gmail.com>
2017-08-29 20:23:08 +0200

    html animals and drops

--- js/game.js
+++ js/game.js
@@ -9,8 +9,14 @@ Game.prototype.addPile = function(animals) {
   console.log(this.animals);
 
   for( var i = 1; i < this.animals.length; i++ ) {
-    $('<div>' + this.animals[i].name + '</div>').data( 'animal', this.animals[i].name ).attr( 'id', 'animal-'+ this.animals[i].name ).appendTo( '#cardPile' ).draggable( {
-      containment: '#content',
+    var grades = Math.random() * 45;
+
+    $('<div>' + this.animals[i].name + '</div>')
+    .data( 'animal', this.animals[i].name )
+    .attr( 'id', 'animal-'+ this.animals[i].name )
+    .appendTo( '#cardPile' )
+    .draggable( {
+      containment: '#universe',
       stack: '#cardPile div',
       cursor: 'move',
       revert: true
@@ -23,7 +29,7 @@ Game.prototype.addDrops = function() {
 var extinctionDrop = [ "danger", "no-danger", "extinct", "super" ];
 
   for( var i = 0; i < extinctionDrop.length; i++ ) {
-    $('<div>' + extinctionDrop[i] + '</div>').data( 'number', i ).appendTo( '#cardSlots' ).droppable( {
+    $('<div>' + extinctionDrop[i] + '</div>').data( 'number', i ).attr( 'class', 'col-md-3' ).appendTo( '#cardSlots' ).droppable( {
       accept: '#cardPile div',
       hoverClass: 'hovered',
       //drop: handleCardDrop
