695111ed87d5f1eca7830dd887c25c3ac05d5cd5
Mercedes Egido <meegido@gmail.com>
2017-08-31 13:31:32 +0200

    set interval method for game constructor

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ function Game(animals, player, cards, score) {
   this.player = player;
   this.cards = cards;
   this.score = score;
+  this.count = 10;
 }
 
 
@@ -12,12 +13,32 @@ Game.prototype.startGame = function(animals, cards) {
   cards.addAnimalsPile(animals);
   cards.addDrops();
   this.showScore();
+  this.countDown();
 }
 
 Game.prototype.showScore = function() {
   $("#score").text(player.score)
 }
 
-Game.prototype.reStart = function() {
+
+Game.prototype.countDown = function() {
+  var that = this;
+  var timmer = setInterval(function(){
+    console.log(that.count)
+    that.count--
+    if(that.count < 0) {
+      clearInterval(timmer)
+    }
+  }, 1000)
+
+  //llamo aquí a render para pintar la cuenta atrás
+}
+
+
+Game.prototype.render = function() {
+  
+}
+
+Game.prototype.reStart = function(count) {
 
 }
--- js/index.js
+++ js/index.js
@@ -5,4 +5,18 @@ var game = new Game(animals, player, cards, player.score);
 
 $(document).ready(function(){
   game.startGame(animals, cards, player.score);
+
+
+  // var counter = 10;
+  //
+  // function timmerDown() {
+  //   console.log("hola", counter)
+  //   counter --
+  //   if(counter < 0) {
+  //     clearInterval(timer)
+  //   }
+  // }
+  //
+  // var timer = setInterval(timmerDown, 1000);
+
 });
